73bc903c518554246c9b52b829b2c80935a3dca7
Pradeep Nayak <pradeep1288@gmail.com>
2012-07-04 02:03:59 +0530

    Updating server logic per :http://thecodinghumanist.com/blog/archives/2011/5/6/serving-static-files-from-node-js

--- server.js
+++ server.js
@@ -1,14 +1,50 @@
-var http = require('http'),
-    fs   = require('fs');
-server = http.createServer(function (req, res) {
-  fs.readFile('gapshap.html', function(err, data){
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    res.end();
-  });
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+ 
+server = http.createServer(function (request, response) {
+ 
+    console.log('request starting...');
+     
+    var filePath = '.' + request.url;
+    if (filePath == './')
+        filePath = './gapshap.html';
+         
+    var extname = path.extname(filePath);
+    var contentType = 'text/html';
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+    }
+     
+    path.exists(filePath, function(exists) {
+     
+        if (exists) {
+            fs.readFile(filePath, function(error, content) {
+                if (error) {
+                    response.writeHead(500);
+                    response.end();
+                }
+                else {
+                    response.writeHead(200, { 'Content-Type': contentType });
+                    response.end(content, 'utf-8');
+                }
+            });
+        }
+        else {
+            response.writeHead(404);
+            response.end();
+        }
+    });
+     
 });
 server.listen(8000);
-console.log('Server running at http://127.0.0.1:1337/');
+ 
+console.log('Server running at http://127.0.0.1:8125/');
 
 
 var everyone = require("now").initialize(server);
