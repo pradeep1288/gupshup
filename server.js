6d1c8a234643abf91ba16301f3a4fbccddd54f9a
Pradeep Nayak <pradeep1288@gmail.com>
2012-07-10 00:33:38 +0530

    server runs on port 8000

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ server = http.createServer(function (request, response) {
 });
 server.listen(8000);
  
-console.log('Server running at http://127.0.0.1:8125/');
+console.log('Server running at http://127.0.0.1:8000/');
 
  var nowjs = require('now');
  var everyone = nowjs.initialize(server);
